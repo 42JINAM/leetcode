3775d44b725d2475208987f19e4ab0a3a0f7c57f
42JINAM <158562186+42JINAM@users.noreply.github.com>
2024-12-04 23:28:23 +0100

    Time: 0 ms (100%), Space: 23.9 MB (11.6%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-        int count = 0;
         int prev = 0;
         int next = 0;
         int current;
@@ -13,12 +12,12 @@ public:
                 next = flowerbed[i + 1];
             }
             if (current == 0 && prev == 0 && next == 0) {
-                count++;
+                n --;
                 prev = 1;
             } else {
                 prev = current;
             }
         }
-        return (count >= n);
+        return (n <= 0);
     }
 };
